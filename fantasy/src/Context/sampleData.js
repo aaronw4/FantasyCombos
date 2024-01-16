cf41b38f6967addaab454240f833bff10c1ce22d
Aaron Williams <aaronrw4@yahoo.com>
2024-01-16 17:00:24 -0600

    Created sample data

---
+++ fantasy/src/Context/sampleData.js
@@ -0,0 +1,98 @@
+export const data = [
+    {
+        name: 'Allen',
+        fppg: 25.7,
+        oprk: 4,
+        salary: 8000
+    },
+    {
+        name: 'Mahomes',
+        fppg: 19.3,
+        oprk: 6,
+        salary: 6800
+    },
+    {
+        name: 'Pacheco',
+        fppg: 16,
+        oprk: 16,
+        salary: 6400
+    },
+    {
+        name: 'Cook',
+        fppg: 14.2,
+        oprk: 9,
+        salary: 6300
+    },
+    {
+        name: 'Diggs',
+        fppg: 16.8,
+        oprk: 3,
+        salary: 7000
+    },
+    {
+        name: 'Rice',
+        fppg: 14.7,
+        oprk: 8,
+        salary: 6800
+    },
+    {
+        name: 'Davis',
+        fppg: 10.1,
+        oprk: 3,
+        salary: 5000
+    },
+    {
+        name: 'Shakir',
+        fppg: 7.3,
+        oprk: 3,
+        salary: 3800
+    },
+    {
+        name: 'Watson',
+        fppg: 5.5,
+        oprk: 8,
+        salary: 3300
+    },
+    {
+        name: 'Valdes',
+        fppg: 3.5,
+        oprk: 8,
+        salary: 3000
+    },
+    {
+        name: 'Toney',
+        fppg: 4.1,
+        oprk: 8,
+        salary: 3200
+    },
+    {
+        name: 'Harty',
+        fppg: 2.7,
+        oprk: 3,
+        salary: 3000
+    },
+    {
+        name: 'Kelce',
+        fppg: 15,
+        oprk: 4,
+        salary: 6000
+    },
+    {
+        name: 'Kincaid',
+        fppg: 9.8,
+        oprk: 7,
+        salary: 4800
+    },
+    {
+        name: 'Bills',
+        fppg: 9.2,
+        oprk: 16,
+        salary: 2800
+    },
+    {
+        name: 'Chiefs',
+        fppg: 7.9,
+        oprk: 3,
+        salary: 2700
+    }
+]
