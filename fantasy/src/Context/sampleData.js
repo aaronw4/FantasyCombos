76542a7d96e0d582ef310eaf613e87d41a577827
Aaron Williams <aaronrw4@yahoo.com>
2024-01-19 15:19:40 -0600

    updated data

--- fantasy/src/Context/sampleData.js
+++ fantasy/src/Context/sampleData.js
@@ -3,83 +3,71 @@ export const data = [
         name: 'Allen',
         fppg: 25.7,
         oprk: 4,
-        salary: 8000
+        salary: 12200
     },
     {
         name: 'Mahomes',
         fppg: 19.3,
         oprk: 6,
-        salary: 6800
+        salary: 10600
     },
     {
         name: 'Pacheco',
         fppg: 16,
         oprk: 16,
-        salary: 6400
+        salary: 10400
     },
     {
         name: 'Cook',
         fppg: 14.2,
         oprk: 9,
-        salary: 6300
+        salary: 8200
     },
     {
         name: 'Diggs',
         fppg: 16.8,
         oprk: 3,
-        salary: 7000
+        salary: 8800
     },
     {
         name: 'Rice',
         fppg: 14.7,
         oprk: 8,
-        salary: 6800
-    },
-    {
-        name: 'Davis',
-        fppg: 10.1,
-        oprk: 3,
-        salary: 5000
+        salary: 9600
     },
     {
         name: 'Shakir',
         fppg: 7.3,
         oprk: 3,
-        salary: 3800
+        salary: 5400
     },
     {
         name: 'Watson',
         fppg: 5.5,
         oprk: 8,
-        salary: 3300
-    },
-    {
-        name: 'Valdes',
-        fppg: 3.5,
-        oprk: 8,
-        salary: 3000
-    },
-    {
-        name: 'Toney',
-        fppg: 4.1,
-        oprk: 8,
-        salary: 3200
-    },
-    {
-        name: 'Harty',
-        fppg: 2.7,
-        oprk: 3,
-        salary: 3000
+        salary: 4400
     },
     {
         name: 'Kelce',
         fppg: 15,
         oprk: 4,
-        salary: 6000
+        salary: 7800
     },
     {
         name: 'Kincaid',
         fppg: 9.8,
+        oprk: 7,
+        salary: 6400
+    },
+    {
+        name: 'Bass',
+        fppg: 7.8,
+        oprk: 1,
+        salary: 5000
+    },
+    {
+        name: 'Butker',
+        fppg: 9.3,
         oprk: 7,
         salary: 4800
     },
@@ -87,12 +75,12 @@ export const data = [
         name: 'Bills',
         fppg: 9.2,
         oprk: 16,
-        salary: 2800
+        salary: 4000
     },
     {
         name: 'Chiefs',
         fppg: 7.9,
         oprk: 3,
-        salary: 2700
+        salary: 3800
     }
 ]
