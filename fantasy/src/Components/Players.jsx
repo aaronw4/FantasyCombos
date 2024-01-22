f75cc49643abea7c56fcff6aa44c5fcfe2a3d676
Aaron Williams <aaronrw4@yahoo.com>
2024-01-22 15:43:46 -0600

    Added ability to load sample data and to clear players already loaded

--- fantasy/src/Components/Players.jsx
+++ fantasy/src/Components/Players.jsx
@@ -1,8 +1,21 @@
+import { data } from '../Context/sampleData';
 import { useContext } from "react";
 import PlayerContext from '../Context/playersContext';
 
 export default function Players() {
     const { players, setPlayers, rowNumbers, setRowNumbers } = useContext(PlayerContext);
+
+    function loadSamplePlayers(e) {
+        e.preventDefault();
+        setPlayers(data);
+        setRowNumbers([]);
+    };
+
+    function clearPlayers(e) {
+        e.preventDefault();
+        setPlayers([]);
+        setRowNumbers([]);
+    }
     
     function handleCheckboxChange(e) {
         let newRowNumbers = [...rowNumbers];
@@ -26,7 +39,9 @@ export default function Players() {
     };
 
     return (
-        <>
+        <div id="playerCont">
+            <button onClick={loadSamplePlayers}>Load Sample Data</button>
+            <button onClick={clearPlayers}>Clear Players</button>
             <table>
                 <thead>
                     <tr>
@@ -60,6 +75,6 @@ export default function Players() {
                     )}
                 </tbody>
             </table>
-        </>
+        </div>
     );
 };
