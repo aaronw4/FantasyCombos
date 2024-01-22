ad5cddea6ff56ae4f3814c1a81de932ad113b832
Aaron Williams <aaronrw4@yahoo.com>
2024-01-22 15:43:02 -0600

    Added id's to sample data

--- fantasy/src/Context/sampleData.js
+++ fantasy/src/Context/sampleData.js
@@ -1,83 +1,97 @@
 export const data = [
     {
+        id: "1",
         name: 'Allen',
         fppg: 25.7,
         oprk: 4,
         salary: 12200
     },
     {
+        id: "2",
         name: 'Mahomes',
         fppg: 19.3,
         oprk: 6,
         salary: 10600
     },
     {
+        id: "3",
         name: 'Pacheco',
         fppg: 16,
         oprk: 16,
         salary: 10400
     },
     {
+        id: "4",
         name: 'Cook',
         fppg: 14.2,
         oprk: 9,
         salary: 8200
     },
     {
+        id: "5",
         name: 'Diggs',
         fppg: 16.8,
         oprk: 3,
         salary: 8800
     },
     {
+        id: "6",
         name: 'Rice',
         fppg: 14.7,
         oprk: 8,
         salary: 9600
     },
     {
+        id: "7",
         name: 'Shakir',
         fppg: 7.3,
         oprk: 3,
         salary: 5400
     },
     {
+        id: "8",
         name: 'Watson',
         fppg: 5.5,
         oprk: 8,
         salary: 4400
     },
     {
+        id: "9",
         name: 'Kelce',
         fppg: 15,
         oprk: 4,
         salary: 7800
     },
     {
+        id: "10",
         name: 'Kincaid',
         fppg: 9.8,
         oprk: 7,
         salary: 6400
     },
     {
+        id: "12",
         name: 'Bass',
         fppg: 7.8,
         oprk: 1,
         salary: 5000
     },
     {
+        id: "13",
         name: 'Butker',
         fppg: 9.3,
         oprk: 7,
         salary: 4800
     },
     {
+        id: "14",
         name: 'Bills',
         fppg: 9.2,
         oprk: 16,
         salary: 4000
     },
     {
+        id: "15",
         name: 'Chiefs',
         fppg: 7.9,
         oprk: 3,
