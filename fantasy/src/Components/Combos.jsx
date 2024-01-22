2f120439336f8ad6660530b7961695aa81b9ecf3
Aaron Williams <aaronrw4@yahoo.com>
2024-01-22 15:44:55 -0600

    Fixed bug in generating combos and created jsx team cards

--- fantasy/src/Components/Combos.jsx
+++ fantasy/src/Components/Combos.jsx
@@ -8,33 +8,38 @@ export default function Combos() {
     for (let i = 0; i < players.length; i++) {
         let team = [];
         let player = structuredClone(players[i]);
-        
+
         if (rowNumbers.includes(player.id)) {
             let newPlayers = [...players];
             player.salary = player.salary * 1.5;
             team.push(player);
             newPlayers.splice(i, 1);
-            
+
             buildTeams(team, newPlayers);
         }
     }
 
     function buildTeams(team, playersRemaining) {
-        for (let i = 0; i < playersRemaining.length; i++) {
-            let cost = team.reduce((n, {salary}) => n + salary, 0);
+        let cost = team.reduce((n, {salary}) => n + salary, 0);
 
-            if (cost > 50000) {
+        if (cost > 50000) {
+            return
+        } else if (playersRemaining.length === 0 && team.length === 6) {
+            team.push(cost);
+            teamCombos.push(team);
+            return
+        }
+
+        for (let i = 0; i < playersRemaining.length; i++) {
+            if (team.length === 6) {
+                team.push(cost);
+                teamCombos.push(team);
                 return
-            } else if (team.length === 6) {
-                team.push(cost)
-                teamCombos.push(team)
-                return
-            } else if (playersRemaining.length === 0) {
-                return
-            }
+            } 
 
             let newTeam = [...team];
             let playersList = structuredClone(playersRemaining);
+            
             newTeam.push(playersList[i]);
             let remainingPlayers = playersList.slice(i+1);
 
@@ -43,9 +48,30 @@ export default function Combos() {
     };
 
     return (
-        <>
-            <p>{teamCombos.length} Teams</p>
-            {console.log(teamCombos)}
-        </>
+        <div id="teamCombosCont">
+            {
+                teamCombos.length === 0 ? null :
+                <p>{teamCombos.length} Teams</p>
+            }
+            <div id='teamCombos'>
+                {teamCombos.map(team => 
+                    <div className="team">
+                        <tbody>
+                            {team.map(player => 
+                                player.name ? 
+                                    <tr className='teamRow'>
+                                        <td>{player.name}</td>
+                                        <td>{player.fppg} </td>
+                                        <td>{player.oprk}</td> 
+                                        <td>${player.salary}</td>
+                                    </tr> 
+                                : null    
+                            )}
+                            <p>Total Cost: ${team[6]}</p>
+                        </tbody>
+                    </div>
+                )}
+            </div>
+        </div>
     )
 };
